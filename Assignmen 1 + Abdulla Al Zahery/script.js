d60f88a98bfbd82070a9d2b9d82b239e3a7e8e5e
abdullaal1 <abdulla.alzahery@gritacademy.se>
2025-05-08 22:53:50 +0200

    Add files via upload

---
+++ Assignmen 1 + Abdulla Al Zahery/script.js
@@ -0,0 +1,17 @@
+ alert ( "Hello" ) 
+document.write ( 456 )
+console.log ( 789 )
+
+
+
+foo = 1-1
+bar = 1*0
+
+
+
+
+
+// this is a comment
+
+
+
---
+++ Assignmen 1 + Abdulla Al Zahery/script/script.js
@@ -0,0 +1,11 @@
+ alert ( "Hello" ) 
+document.write ( 456 )
+console.log ( 789 )
+
+
+
+foo = 1-1
+let bar = 1*0
+
+
+
